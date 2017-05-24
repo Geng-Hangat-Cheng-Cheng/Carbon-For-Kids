a8eae513554f7580a370e4d152e2a9af923bb4ea
SeladangHijau <nadzmiidzham@gmail.com>
2017-05-24 23:50:13 +0600

    Simple update
    Update:
    - Added script to go to another scene when clicked
    - added level menu background
    - added animated button for each level
    - added simple UI

---
+++ Assets/script/level_menu_scene/LevelMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelMenu : MonoBehaviour {
+    public string sceneLvlName;
+
+    private void OnMouseDown()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+            // SceneManager.LoadScene(sceneLvlName);
+            Debug.Log("Go to scene: " + sceneLvlName);
+    }
+}
