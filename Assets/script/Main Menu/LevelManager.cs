903ad74bb76a5218951f69aad04a99c860f95d26
wawarina <nurshazwarina@rocketmail.com>
2017-05-26 00:12:15 +0600

    update 1
    D-main menu
    >home button
    >settings
    >about
    -link panel to panel

---
+++ Assets/script/Main Menu/LevelManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+    
+    //ref object 
+    public Transform mainMenu, settingsMenu, aboutMenu;
+
+    //load levela
+    public void LoadScene(string name)
+    {
+        Application.LoadLevel(name);
+    }
+
+    //setting button change
+    public void SettingsMenu(bool clicked)
+    {
+        if(clicked == true)
+        {
+            settingsMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(false);
+        }
+        else
+        {
+            settingsMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(true);
+        }
+    }
+
+    //about button change
+    public void AboutMenu(bool clicked)
+    {
+        if(clicked == true)
+        {
+            aboutMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(false);
+        }
+        else
+        {
+            aboutMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(true);
+        }
+    }
+
+}
