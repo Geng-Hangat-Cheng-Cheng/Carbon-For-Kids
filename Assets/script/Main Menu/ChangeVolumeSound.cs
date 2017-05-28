450db331eb2fbafadd13d9f1b6491c63014baad8
wawarina <nurshazwarina@rocketmail.com>
2017-05-29 01:00:51 +0600

    slider sound + bgm

---
+++ Assets/script/Main Menu/ChangeVolumeSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeVolumeSound : MonoBehaviour {
+
+    public Slider Volume;
+    public AudioSource myMusic;
+	
+	// Update is called once per frame
+	public void Update () {
+        myMusic.volume = Volume.value;
+	}
+}
